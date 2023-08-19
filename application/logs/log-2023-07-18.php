6fd82c8e58e6e0b5ea0a75e6e4d3106757cf728c
Kelvin Eger <kelvin.eger@hotmail.com>
2023-08-18 21:11:28 -0300

    Initial commit

---
+++ application/logs/log-2023-07-18.php
@@ -0,0 +1,740 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-07-18 19:12:12 --> 220 smtp.gmail.com ESMTP eb2-20020a056870a88200b001b03e0c0970sm1397945oab.2 - gsmtp
+<br /><pre>hello: 250-smtp.gmail.com at your service, [2804:d57:470e:c900:45d9:e524:d47a:debb]
+250-SIZE 35882577
+250-8BITMIME
+250-STARTTLS
+250-ENHANCEDSTATUSCODES
+250-PIPELINING
+250-CHUNKING
+250 SMTPUTF8
+</pre><pre>starttls: 220 2.0.0 Ready to start TLS
+</pre><pre>hello: 250-smtp.gmail.com at your service, [2804:d57:470e:c900:45d9:e524:d47a:debb]
+250-SIZE 35882577
+250-8BITMIME
+250-AUTH LOGIN PLAIN XOAUTH2 PLAIN-CLIENTTOKEN OAUTHBEARER XOAUTH
+250-ENHANCEDSTATUSCODES
+250-PIPELINING
+250-CHUNKING
+250 SMTPUTF8
+</pre>Failed to authenticate password. Error: 534-5.7.9 Application-specific password required. Learn more at
+534 5.7.9  https://support.google.com/mail/?p=InvalidSecondFactor eb2-20020a056870a88200b001b03e0c0970sm1397945oab.2 - gsmtp
+<br />Unable to send email using PHP SMTP. Your server might not be configured to send mail using this method.<br /><pre>From: kelvin.eger@gmail.com
+Subject: Ordem de Serviço - Criada
+To: kelvin.eger@gmail.com
+Reply-To: &lt;kelvin.eger@gmail.com&gt;
+User-Agent: CodeIgniter
+X-Sender: kelvin.eger@gmail.com
+X-Mailer: CodeIgniter
+X-Priority: 3 (Normal)
+Message-ID: &lt;64b70e3964929&gt;
+Mime-Version: 1.0
+
+
+Content-Type: multipart/alternative; boundary=&quot;B_ALT_64b70e3964949&quot;
+
+This is a multi-part message in MIME format.
+Your email application may not support this format.
+
+--B_ALT_64b70e3964949
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+
+OS #: 1
+ Data Inicial: 18/07/2023 
+ Data Final: 18/07/2023 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ Cliente: Kelvin Eger 09061376912
+ Rua Guilherme Hamann, 738, Fundo Canoas
+ Rio Do Sul - SC 
+ kelvin.eger@gmail.com 
+ (47)91284-658 
+
+ 
+ Kelvin Eger 09061376912 
+ Rua Guilherme Hamann, 738, Fundo Canoas
+ Rio Do Sul - SC CEP: 89163-452 
+ Responsável: Kelvin Eger
+ kelvin.eger@gmail.com 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ Status
+ 
+ 
+ Orçamento 
+ 
+
+ 
+ 
+ 
+ Descrição
+ 
+ 
+ 
+ 
+ dasda 
+ 
+ 
+ 
+ 
+ Defeito Apresentado
+ 
+ 
+ 
+ 
+ ada 
+ 
+ 
+ 
+ 
+ Observações
+ 
+ 
+ 
+ 
+ asdasd 
+ 
+ 
+ 
+ 
+ Laudo Técnico
+ 
+ 
+ 
+ 
+ adasdas 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ Total: R$ 0,00
+
+--B_ALT_64b70e3964949
+Content-Type: text/html; charset=UTF-8
+Content-Transfer-Encoding: quoted-printable
+
+
+=3C=21doctype html=3E
+=3Chtml=3E
+
+=3Chead=3E
+ =3Cmeta charset=3D=22utf-8=22=3E
+ =3Cstyle=3E
+ .invoice-box =7B
+ max-width: 1100px=3B
+ margin: auto=3B
+ padding: 10px=3B
+ border: 1px solid =23eee=3B
+ box-shadow: 0 0 10px rgba(0, 0, 0, .15)=3B
+ font-size: 16px=3B
+ line-height: 24px=3B
+ font-family: &#039;Helvetica Neue&#039;, &#039;Helvetica&#039;, Helvetica, Arial, sans-serif=
+=3B
+ color: =23555=3B
+ =7D
+
+ .invoice-box table =7B
+ width: 100=25=3B
+ line-height: inherit=3B
+ text-align: left=3B
+ =7D
+
+ .invoice-box table td =7B
+ padding: 5px=3B
+ vertical-align: top=3B
+ =7D
+
+ .invoice-box table tr td:nth-child(2) =7B
+ text-align: right=3B
+ =7D
+
+ .invoice-box table tr.top table td =7B
+ padding-bottom: 20px=3B
+ =7D
+
+ .invoice-box table tr.top table td.title =7B
+ font-size: 45px=3B
+ line-height: 45px=3B
+ color: =23333=3B
+ =7D
+
+ .invoice-box table tr.information table td =7B
+ padding-bottom: 40px=3B
+ =7D
+
+ .invoice-box table tr.heading td =7B
+ background: =23eee=3B
+ border-bottom: 1px solid =23ddd=3B
+ font-weight: bold=3B
+ =7D
+
+ .invoice-box table tr.details td =7B
+ padding-bottom: 20px=3B
+ =7D
+
+ .invoice-box table tr.item td =7B
+ border-bottom: 1px solid =23eee=3B
+ =7D
+
+ .invoice-box table tr.item.last td =7B
+ border-bottom: none=3B
+ =7D
+
+ .invoice-box table tr.total td:nth-child(2) =7B
+ border-top: 2px solid =23eee=3B
+ font-weight: bold=3B
+ =7D
+
+ =40media only screen and (max-width: 600px) =7B
+ .invoice-box table tr.top table td =7B
+ width: 100=25=3B
+ display: block=3B
+ text-align: center=3B
+ =7D
+
+ .invoice-box table tr.information table td =7B
+ width: 100=25=3B
+ display: block=3B
+ text-align: center=3B
+ =7D
+ =7D
+
+ /=2A=2A RTL =2A=2A/
+ .rtl =7B
+ direction: rtl=3B
+ font-family: Tahoma, &#039;Helvetica Neue&#039;, &#039;Helvetica&#039;, Helvetica, Arial, sans=
+-serif=3B
+ =7D
+
+ .rtl table =7B
+ text-align: right=3B
+ =7D
+
+ .rtl table tr td:nth-child(2) =7B
+ text-align: left=3B
+ =7D
+
+ .justify =7B
+ text-align: justify=3B
+ =7D
+ =3C/style=3E
+=3C/head=3E
+
+=3Cbody=3E
+ =3Cdiv class=3D=22invoice-box=22=3E
+ =3Ctable cellpadding=3D=220=22 cellspacing=3D=220=22=3E
+ =3Ctr class=3D=22top=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Ctable=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22title=22=3E
+ =3Cimg src=3D=22http://localhost/agileeasy/assets/uploads/1d4b7392308ef698=
+1316374262514572.png=22 style=3D=22width:100=25=3B max-width:120px=3B=22=3E
+ =3C/td=3E
+ =3Ctd style=3D=22text-align: right=22=3E
+ OS =23: 1=3Cbr=3E
+ Data Inicial: 18/07/2023 =3Cbr=3E
+ Data Final: 18/07/2023 =3C/td=3E
+ =3C/tr=3E
+ =3C/table=3E
+ =3C/td=3E
+ =3C/tr=3E
+
+ =3Ctr class=3D=22information=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Ctable=3E
+ =3Ctr=3E
+ =3Ctd=3E
+ Cliente: Kelvin Eger 09061376912=3Cbr=3E
+ Rua Guilherme Hamann, 738, Fundo Canoas=3Cbr=3E
+ Rio Do Sul - SC =3Cbr=3E
+ kelvin.eger=40gmail.com =3Cbr=3E
+ (47)91284-658 =3C/td=3E
+
+ =3Ctd style=3D=22text-align: right=22=3E
+ Kelvin Eger 09061376912 =3Cbr=3E
+ Rua Guilherme Hamann, 738, Fundo Canoas=3Cbr=3E
+ Rio Do Sul - SC CEP: 89163-452 =3Cbr=3E
+ Respons=C3=A1vel: Kelvin Eger=3Cbr=3E
+ kelvin.eger=40gmail.com =3C/td=3E
+ =3C/tr=3E
+ =3C/table=3E
+ =3C/td=3E
+ =3C/tr=3E
+
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=222=22=3E
+ Status
+ =3C/td=3E
+ =3Ctd colspan=3D=222=22 style=3D=22text-align: center=22=3E
+ Or=C3=A7amento =3C/td=3E
+ =3C/tr=3E
+
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cb=3EDescri=C3=A7=C3=A3o=3C/b=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22justify=22 colspan=3D=224=22=3E
+ =3Cp=3Edasda=3C/p=3E =3C/td=3E
+ =3C/tr=3E
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cb=3EDefeito Apresentado=3C/b=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22justify=22 colspan=3D=224=22=3E
+ =3Cp=3Eada=3C/p=3E =3C/td=3E
+ =3C/tr=3E
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cb=3EObserva=C3=A7=C3=B5es=3C/b=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22justify=22 colspan=3D=224=22=3E
+ =3Cp=3Easdasd=3C/p=3E =3C/td=3E
+ =3C/tr=3E
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cb=3ELaudo T=C3=A9cnico=3C/b=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22justify=22 colspan=3D=224=22=3E
+ =3Cp=3Eadasdas=3C/p=3E =3C/td=3E
+ =3C/tr=3E
+=20
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cbr=3E
+ =3C/td=3E
+ =3C/tr=3E
+
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=223=22=3E=3C/td=3E
+ =3Ctd style=3D=22text-align: center=22=3E
+ =3Cstrong=3ETotal: R=24 0,00=3C/strong=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3C/table=3E
+ =3C/div=3E
+=3C/body=3E
+
+=3C/html=3E
+
+--B_ALT_64b70e3964949--</pre>
+ERROR - 2023-07-18 19:12:12 --> 220 smtp.gmail.com ESMTP eb2-20020a056870a88200b001b03e0c0970sm1397945oab.2 - gsmtp
+<br /><pre>hello: 250-smtp.gmail.com at your service, [2804:d57:470e:c900:45d9:e524:d47a:debb]
+250-SIZE 35882577
+250-8BITMIME
+250-STARTTLS
+250-ENHANCEDSTATUSCODES
+250-PIPELINING
+250-CHUNKING
+250 SMTPUTF8
+</pre><pre>starttls: 220 2.0.0 Ready to start TLS
+</pre><pre>hello: 250-smtp.gmail.com at your service, [2804:d57:470e:c900:45d9:e524:d47a:debb]
+250-SIZE 35882577
+250-8BITMIME
+250-AUTH LOGIN PLAIN XOAUTH2 PLAIN-CLIENTTOKEN OAUTHBEARER XOAUTH
+250-ENHANCEDSTATUSCODES
+250-PIPELINING
+250-CHUNKING
+250 SMTPUTF8
+</pre>Failed to authenticate password. Error: 534-5.7.9 Application-specific password required. Learn more at
+534 5.7.9  https://support.google.com/mail/?p=InvalidSecondFactor eb2-20020a056870a88200b001b03e0c0970sm1397945oab.2 - gsmtp
+<br />Unable to send email using PHP SMTP. Your server might not be configured to send mail using this method.<br />Failed to authenticate password. Error: 534-5.7.9 Application-specific password required. Learn more at
+534 5.7.9  https://support.google.com/mail/?p=InvalidSecondFactor eb2-20020a056870a88200b001b03e0c0970sm1397945oab.2 - gsmtp
+<br />Unable to send email using PHP SMTP. Your server might not be configured to send mail using this method.<br /><pre>From: kelvin.eger@gmail.com
+Subject: Ordem de Serviço
+To: kelvin.eger@gmail.com
+User-Agent: CodeIgniter
+X-Sender: kelvin.eger@gmail.com
+X-Mailer: CodeIgniter
+X-Priority: 3 (Normal)
+Message-ID: &lt;64b70e3c037b2&gt;
+Mime-Version: 1.0
+
+
+Content-Type: multipart/alternative; boundary=&quot;B_ALT_64b70e3c037f7&quot;
+
+This is a multi-part message in MIME format.
+Your email application may not support this format.
+
+--B_ALT_64b70e3c037f7
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+
+OS #: 1
+ Data Inicial: 18/07/2023 
+ Data Final: 18/07/2023 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ Cliente: Kelvin Eger 09061376912
+ Rua Guilherme Hamann, 738, Fundo Canoas
+ Rio Do Sul - SC 
+ kelvin.eger@gmail.com 
+ (47)91284-658 
+
+ 
+ Kelvin Eger 09061376912 
+ Rua Guilherme Hamann, 738, Fundo Canoas
+ Rio Do Sul - SC CEP: 89163-452 
+ Responsável: Kelvin Eger
+ kelvin.eger@gmail.com 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ Status
+ 
+ 
+ Orçamento 
+ 
+
+ 
+ 
+ 
+ Descrição
+ 
+ 
+ 
+ 
+ dasda 
+ 
+ 
+ 
+ 
+ Defeito Apresentado
+ 
+ 
+ 
+ 
+ ada 
+ 
+ 
+ 
+ 
+ Observações
+ 
+ 
+ 
+ 
+ asdasd 
+ 
+ 
+ 
+ 
+ Laudo Técnico
+ 
+ 
+ 
+ 
+ adasdas 
+ 
+ 
+ 
+ 
+ 
+ Serviço
+ Quantidade
+ Preço unit.
+ Sub-total
+ 
+
+ serviço demonstração112.31R$ 12,31
+ 
+ 
+ Total em Serviços: R$ 12,31
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ Total: R$ 12,31
+
+--B_ALT_64b70e3c037f7
+Content-Type: text/html; charset=UTF-8
+Content-Transfer-Encoding: quoted-printable
+
+
+=3C=21doctype html=3E
+=3Chtml=3E
+
+=3Chead=3E
+ =3Cmeta charset=3D=22utf-8=22=3E
+ =3Cstyle=3E
+ .invoice-box =7B
+ max-width: 1100px=3B
+ margin: auto=3B
+ padding: 10px=3B
+ border: 1px solid =23eee=3B
+ box-shadow: 0 0 10px rgba(0, 0, 0, .15)=3B
+ font-size: 16px=3B
+ line-height: 24px=3B
+ font-family: &#039;Helvetica Neue&#039;, &#039;Helvetica&#039;, Helvetica, Arial, sans-serif=
+=3B
+ color: =23555=3B
+ =7D
+
+ .invoice-box table =7B
+ width: 100=25=3B
+ line-height: inherit=3B
+ text-align: left=3B
+ =7D
+
+ .invoice-box table td =7B
+ padding: 5px=3B
+ vertical-align: top=3B
+ =7D
+
+ .invoice-box table tr td:nth-child(2) =7B
+ text-align: right=3B
+ =7D
+
+ .invoice-box table tr.top table td =7B
+ padding-bottom: 20px=3B
+ =7D
+
+ .invoice-box table tr.top table td.title =7B
+ font-size: 45px=3B
+ line-height: 45px=3B
+ color: =23333=3B
+ =7D
+
+ .invoice-box table tr.information table td =7B
+ padding-bottom: 40px=3B
+ =7D
+
+ .invoice-box table tr.heading td =7B
+ background: =23eee=3B
+ border-bottom: 1px solid =23ddd=3B
+ font-weight: bold=3B
+ =7D
+
+ .invoice-box table tr.details td =7B
+ padding-bottom: 20px=3B
+ =7D
+
+ .invoice-box table tr.item td =7B
+ border-bottom: 1px solid =23eee=3B
+ =7D
+
+ .invoice-box table tr.item.last td =7B
+ border-bottom: none=3B
+ =7D
+
+ .invoice-box table tr.total td:nth-child(2) =7B
+ border-top: 2px solid =23eee=3B
+ font-weight: bold=3B
+ =7D
+
+ =40media only screen and (max-width: 600px) =7B
+ .invoice-box table tr.top table td =7B
+ width: 100=25=3B
+ display: block=3B
+ text-align: center=3B
+ =7D
+
+ .invoice-box table tr.information table td =7B
+ width: 100=25=3B
+ display: block=3B
+ text-align: center=3B
+ =7D
+ =7D
+
+ /=2A=2A RTL =2A=2A/
+ .rtl =7B
+ direction: rtl=3B
+ font-family: Tahoma, &#039;Helvetica Neue&#039;, &#039;Helvetica&#039;, Helvetica, Arial, sans=
+-serif=3B
+ =7D
+
+ .rtl table =7B
+ text-align: right=3B
+ =7D
+
+ .rtl table tr td:nth-child(2) =7B
+ text-align: left=3B
+ =7D
+
+ .justify =7B
+ text-align: justify=3B
+ =7D
+ =3C/style=3E
+=3C/head=3E
+
+=3Cbody=3E
+ =3Cdiv class=3D=22invoice-box=22=3E
+ =3Ctable cellpadding=3D=220=22 cellspacing=3D=220=22=3E
+ =3Ctr class=3D=22top=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Ctable=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22title=22=3E
+ =3Cimg src=3D=22http://localhost/agileeasy/assets/uploads/1d4b7392308ef698=
+1316374262514572.png=22 style=3D=22width:100=25=3B max-width:120px=3B=22=3E
+ =3C/td=3E
+ =3Ctd style=3D=22text-align: right=22=3E
+ OS =23: 1=3Cbr=3E
+ Data Inicial: 18/07/2023 =3Cbr=3E
+ Data Final: 18/07/2023 =3C/td=3E
+ =3C/tr=3E
+ =3C/table=3E
+ =3C/td=3E
+ =3C/tr=3E
+
+ =3Ctr class=3D=22information=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Ctable=3E
+ =3Ctr=3E
+ =3Ctd=3E
+ Cliente: Kelvin Eger 09061376912=3Cbr=3E
+ Rua Guilherme Hamann, 738, Fundo Canoas=3Cbr=3E
+ Rio Do Sul - SC =3Cbr=3E
+ kelvin.eger=40gmail.com =3Cbr=3E
+ (47)91284-658 =3C/td=3E
+
+ =3Ctd style=3D=22text-align: right=22=3E
+ Kelvin Eger 09061376912 =3Cbr=3E
+ Rua Guilherme Hamann, 738, Fundo Canoas=3Cbr=3E
+ Rio Do Sul - SC CEP: 89163-452 =3Cbr=3E
+ Respons=C3=A1vel: Kelvin Eger=3Cbr=3E
+ kelvin.eger=40gmail.com =3C/td=3E
+ =3C/tr=3E
+ =3C/table=3E
+ =3C/td=3E
+ =3C/tr=3E
+
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=222=22=3E
+ Status
+ =3C/td=3E
+ =3Ctd colspan=3D=222=22 style=3D=22text-align: center=22=3E
+ Or=C3=A7amento =3C/td=3E
+ =3C/tr=3E
+
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cb=3EDescri=C3=A7=C3=A3o=3C/b=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22justify=22 colspan=3D=224=22=3E
+ =3Cp=3Edasda=3C/p=3E =3C/td=3E
+ =3C/tr=3E
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cb=3EDefeito Apresentado=3C/b=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22justify=22 colspan=3D=224=22=3E
+ =3Cp=3Eada=3C/p=3E =3C/td=3E
+ =3C/tr=3E
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cb=3EObserva=C3=A7=C3=B5es=3C/b=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22justify=22 colspan=3D=224=22=3E
+ =3Cp=3Easdasd=3C/p=3E =3C/td=3E
+ =3C/tr=3E
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cb=3ELaudo T=C3=A9cnico=3C/b=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3Ctr=3E
+ =3Ctd class=3D=22justify=22 colspan=3D=224=22=3E
+ =3Cp=3Eadasdas=3C/p=3E =3C/td=3E
+ =3C/tr=3E
+=20
+=20
+=20
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd=3EServi=C3=A7o=3C/td=3E
+ =3Ctd=3EQuantidade=3C/td=3E
+ =3Ctd=3EPre=C3=A7o unit.=3C/td=3E
+ =3Ctd style=3D=22text-align: center=22=3ESub-total=3C/td=3E
+ =3C/tr=3E
+
+ =3Ctr class=3D=22item=22=3E=3Ctd=3Eservi=C3=A7o demonstra=C3=A7=C3=A3o=3C/=
+td=3E=3Ctd=3E1=3C/td=3E=3Ctd=3E12.31=3C/td=3E=3Ctd=3ER=24 12,31=3C/td=3E=3C=
+/tr=3E
+ =3Ctr class=3D=22item=22=3E
+ =3Ctd colspan=3D=223=22=3E=3C/td=3E
+ =3Ctd style=3D=22text-align: center=22=3E=3Cstrong=3ETotal em Servi=C3=A7o=
+s: R=24 12,31=3C/strong=3E=3C/td=3E
+ =3C/tr=3E
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=224=22=3E
+ =3Cbr=3E
+ =3C/td=3E
+ =3C/tr=3E
+
+ =3Ctr class=3D=22heading=22=3E
+ =3Ctd colspan=3D=223=22=3E=3C/td=3E
+ =3Ctd style=3D=22text-align: center=22=3E
+ =3Cstrong=3ETotal: R=24 12,31=3C/strong=3E
+ =3C/td=3E
+ =3C/tr=3E
+ =3C/table=3E
+ =3C/div=3E
+=3C/body=3E
+
+=3C/html=3E
+
+--B_ALT_64b70e3c037f7--</pre>
---
+++ application/logs/log-2023-08-16.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-08-16 18:41:42 --> 404 Page Not Found: /index
+ERROR - 2023-08-16 18:41:47 --> 404 Page Not Found: /index
+ERROR - 2023-08-16 18:51:42 --> 404 Page Not Found: Organize/index
